09c8c35e2651e89f04b7ab9fc5f75db7ecd5733e
natalia <dabkowicznatalia@gmail.com>
2026-06-05 22:10:36 +0200

    conversion numpy array into 3d point cloud

--- icpOpen3d.py
+++ icpOpen3d.py
@@ -1,36 +1,25 @@
+import time
 import numpy as np
 import open3d as o3d
 
-def run_open3d_icp(source_np, target_np, max_distance=0.5, max_iteration=50, tolerance =1e-6):
 
-    #numpy data do open3d
-    source = o3d.geometry.PointCloud()
-    source.points = o3d.utility.Vector3dVector(source_np)
+def numpy_to_open3d(points_np, color=None):
+    """
+    converts a numpy array of 3D points into an open3D point cloud
 
-    target = o3d.geometry.PointCloud()
-    target.points = o3d.utility.Vector3dVector(target_np)
+    parameters:
+    points_np: NumPy array with shape (n_points, 3)
+    color: optional RGB color, for example [1, 0, 0]
 
-    #trsnsformation matrix
-    init_transformation = np.eye(4)
-    criteria = o3d.pipelines.registration.ICPConvergenceCriteria(
-        relative_fitness=tolerance,
-        relative_rmse = tolerance,
-        max_iteration=max_iteration,
-    )
+    it returns:
+    Open3D PointCloud object
+    """
 
-    estimation_method = o3d.pipelines.registration.TransformationEstimationPointToPoint()
-    #algorithm
-    result = o3d.pipelines.registration.registration_icp(
-        source,
-        target,
-        max_distance,
-        init_transformation,
-        estimation_method,
-        criteria
-    )
+    point_cloud = o3d.geometry.PointCloud()
+    point_cloud.points = o3d.utility.Vector3dVector(points_np)
 
-    source.transform(result.transformation)
-    transformed_source_np = np.asarray(source.points)
+    if color is not None:
+        point_cloud.paint_uniform_color(color)
 
-    return transformed_source_np, result.fitness, result.inlier_rmse
+    return point_cloud
 
