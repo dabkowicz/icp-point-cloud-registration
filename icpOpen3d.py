38a17b31da4f13c5d114c3e62f75bb600419d06d
natalia <dabkowicznatalia@gmail.com>
2026-06-05 19:10:38 +0200

    open3d algorithm

--- icpOpen3d.py
+++ icpOpen3d.py
@@ -2,15 +2,15 @@ import numpy as np
 import open3d as o3d
 
 def run_open3d_icp(source_np, target_np, max_distance=0.5, max_iteration=50, tolerance =1e-6):
-    """       """
-    #konwersja danych numpy na open3d
+
+    #numpy data do open3d
     source = o3d.geometry.PointCloud()
     source.points = o3d.utility.Vector3dVector(source_np)
 
     target = o3d.geometry.PointCloud()
     target.points = o3d.utility.Vector3dVector(target_np)
 
-    #macierz transformacji
+    #trsnsformation matrix
     init_transformation = np.eye(4)
     criteria = o3d.pipelines.registration.ICPConvergenceCriteria(
         relative_fitness=tolerance,
@@ -19,7 +19,7 @@ def run_open3d_icp(source_np, target_np, max_distance=0.5, max_iteration=50, tol
     )
 
     estimation_method = o3d.pipelines.registration.TransformationEstimationPointToPoint()
-    #algorytm
+    #algorithm
     result = o3d.pipelines.registration.registration_icp(
         source,
         target,
