b3e9740df93c27991e96fb3a28ca06c6e158818e
natalia <dabkowicznatalia@gmail.com>
2026-06-05 22:53:38 +0200

    function of the best transformation between two corresponding point clouds

--- icpNumpy.py
+++ icpNumpy.py
@@ -22,3 +22,35 @@ it returns:
 """
 
 
+def best_fit_transform(source, target):
+
+    centroid_source = np.mean(source, axis=0)
+    centroid_target = np.mean(target, axis=0)
+
+    source_centered = source - centroid_source
+    target_centered = target - centroid_target
+
+    covariance_matrix = source_centered.T @ target_centered
+
+    U, _, Vt = np.linalg.svd(covariance_matrix)
+
+    rotation_matrix = Vt.T @ U.T
+
+    #it prevents reflection (ICP should use rotation not mirror reflection)
+    if np.linalg.det(rotation_matrix) < 0:
+        Vt[-1, :] *= -1
+        rotation_matrix = Vt.T @ U.T
+
+    translation_vector = centroid_target - rotation_matrix @ centroid_source
+
+    return rotation_matrix, translation_vector
+
+"""
+^^^
+computes the best rigid transformation between two corresponding point clouds
+the function estimates a rotation matrix and a translation vector  
+it uses an svd based method to find the rotation that minimizes the mean squared
+distance between matching points
+"""
+
+
